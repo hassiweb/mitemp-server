7652c87396d17c6cdb453408cbff2b15655a8d3e
hassiweb <nryk.hashimoto@gmail.com>
2019-06-23 00:22:23 +0900

    Modify shell scripts to remove containers after run

--- ansible/roles/mitemp/files/run_mitemp.sh
+++ ansible/roles/mitemp/files/run_mitemp.sh
@@ -5,6 +5,6 @@ export MITEMP_DIR="/home/pi/mitemp"
 while read line
 do
   echo $line
-  sudo docker run --net host -v $MITEMP_DIR/data:/data -v /etc/localtime:/etc/localtime:ro hassiweb/mitemp python3 mitemp/mitemp.py --backend bluepy poll $line /data
+  sudo docker run --rm --net host -v $MITEMP_DIR/data:/data -v /etc/localtime:/etc/localtime:ro hassiweb/mitemp python3 mitemp/mitemp.py --backend bluepy poll $line /data
 done < $MITEMP_DIR/mac_list.txt
 
--- ansible/roles/mitemp/files/run_mitemp_sender.sh
+++ ansible/roles/mitemp/files/run_mitemp_sender.sh
@@ -3,5 +3,5 @@
 export MITEMP_DATA="/home/pi/mitemp/data"
 export INFLUXDB_CONF="/home/pi/mitemp/conf"
 
-sudo docker run -v $MITEMP_DATA:/data -v $INFLUXDB_CONF:/app/mitemp-sender/conf hassiweb/mitemp-sender python3 sender.py /data /app/mitemp-sender/conf/influxdb.conf
+sudo docker run --rm -v $MITEMP_DATA:/data -v $INFLUXDB_CONF:/app/mitemp-sender/conf hassiweb/mitemp-sender python3 sender.py /data /app/mitemp-sender/conf/influxdb.conf
 rm $MITEMP_DATA/*.json
